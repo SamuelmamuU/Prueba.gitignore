d90f14305be31f6d71aea24ecd73f2119eb24363
SamuelmamuU <137582192+SamuelmamuU@users.noreply.github.com>
2023-06-27 15:25:30 -0600

    Add files via upload

---
+++ pruebas.py
@@ -0,0 +1,10 @@
+from datetime import date
+date.today()
+print(date.today())
+print("Today's date is: " + str(date.today()))
+
+parsecs = 11
+
+lightyears = parsecs * 3.26
+
+print(str(parsecs) + " parsecs son: " + str(lightyears) + " lightyears")
